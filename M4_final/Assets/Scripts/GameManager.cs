cc6c5ca995ee4b134813c7d022eaa408b30f7991
ktuz9v <kiram.arhitector1@yandex.ru>
2024-04-18 19:54:46 +0300

    Main Menu
    Добавил главное меню без оформления и несколько сцен

---
+++ M4_final/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit(); 
+    }
+    public void StartGameC1()
+    {
+        SceneManager.LoadScene("Chapter1 - Surfase");
+    }
+    public void StartGameC2()
+    {
+        SceneManager.LoadScene("Chapter2 - Bottom of the lab Map 1");
+    }
+}
