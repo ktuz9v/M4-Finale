6baadc41b096e779d45fd125c831e00af6a4f03d
ktuz9v <kiram.arhitector1@yandex.ru>
2024-04-20 10:28:07 +0300

    Player
    Добавил персонажа. Сделал вращение камеры, ходьбу, бег, присед, без анимаций

---
+++ M4_final/Assets/Scripts/Player/Movement/CameraRotation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public Transform CameraHolder;
+
+    public float MinAngle;
+    public float MaxAngle;
+
+    public float RotationSpeed;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CamRotByY();
+        CamRotByX();
+    }
+    private void CamRotByX()
+    {
+        float newAngleY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * RotationSpeed;
+        transform.localEulerAngles = new Vector3(0, newAngleY, 0);
+    }
+    private void CamRotByY()
+    {
+        float newAngleX = CameraHolder.localEulerAngles.x - Input.GetAxis("Mouse Y") * RotationSpeed;
+        if (newAngleX > 180)
+        {
+            newAngleX -= 360;
+        }
+
+        newAngleX = Mathf.Clamp(newAngleX, MinAngle, MaxAngle);
+
+        CameraHolder.localEulerAngles = new Vector3(newAngleX, 0, 0);
+    }
+}
---
+++ M4_final/Assets/Scripts/Player/Movement/PlayerController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    public float gravity = 9.8f;
+    public float jumpForce;
+    public float speed;
+
+    Vector3 _moveVector;
+    float _fallVelocity = 0;
+    bool _isCrouching;
+    bool _isRunning;
+
+    CharacterController _characterController;
+
+    void Start()
+    {
+        ComponentAdd();
+    }
+    private void ComponentAdd()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        MovementForseAdd();
+        Jump();
+        Run();
+        Crouch();
+    }
+    private void Crouch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            _characterController.height = 1;
+            _characterController.center = new Vector3(0, 1.2f, 0);
+            speed = 1f;
+            _isCrouching = true;
+        }
+        else
+        {
+            _characterController.height = 1.7f;
+            _characterController.center = new Vector3(0, 0.85f, 0);
+            if (_isRunning == false)
+                speed = 3f;
+            else
+                speed = 6;
+            _isCrouching = false;
+        }
+    }
+    private void Run()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && _isCrouching == false)
+        {
+            speed = 6;
+            _isRunning = true;
+        }    
+        else
+        {
+            speed = 3;
+            _isRunning = false;
+        }
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = -jumpForce;
+        }
+    }
+    private void MovementForseAdd()
+    {
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+    } //_moveVector change according to WASD
+
+
+    void FixedUpdate()
+    {
+        AplyingMovement();
+        AplyingGravity();
+
+        if (_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        } //Grounded check
+    }
+    private void AplyingGravity()
+    {
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+    }
+    private void AplyingMovement()
+    {
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+    }
+}
