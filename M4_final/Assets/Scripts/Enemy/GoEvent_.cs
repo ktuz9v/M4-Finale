4f5711f776fc3cf76ebfaa9faa82d75a0afc34f8
ktuz9v <kiram.arhitector1@yandex.ru>
2024-05-02 20:21:34 +0300

    Zombie
    Добавил зомби и настроил ему анимации и поведение

---
+++ M4_final/Assets/Scripts/Enemy/GoEvent_.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoEvent_ : MonoBehaviour
+{
+    public ZombieAI ZombieAI;
+    public void GoEvent()
+    {
+        ZombieAI.IsStanding = true;
+    }
+    public void AttackDamage()
+    {
+        ZombieAI.AttackDamage();
+    }
+}
---
+++ M4_final/Assets/Scripts/Enemy/ZombieAI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieAI : MonoBehaviour
+{
+    public float Damage;
+    public NavMeshAgent Zombie;
+    public Animator Animator;
+
+    public Transform Player;
+    public float Distanse;
+
+    public bool IsStanding = false;
+
+    bool _isNotised;
+    void Update()
+    {
+        Notise();
+        if (_isNotised && IsStanding)
+        {
+            Walk();
+            Attack();
+        }
+    }
+    public void AttackDamage()
+    {
+        Player.GetComponent<PlayerHealth>().DealDamageToPlayer(Damage);
+    }
+    private void Attack()
+    {
+        if (Zombie.remainingDistance < 2)
+            Animator.SetTrigger("Attack");
+    }
+    private void Walk()
+    {
+        Zombie.SetDestination(Player.position);
+    }
+    private void Notise()
+    {
+        if (Vector3.Distance(transform.position, Player.position) < Distanse)
+        {
+            Animator.SetTrigger("Awake");
+            _isNotised = true;
+        }
+    }
+}
