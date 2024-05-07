ed4503747d71545c581bb0e155c6afbef301c221
ktuz9v <kiram.arhitector1@yandex.ru>
2024-05-07 19:44:08 +0300

    Chapter 2 Map 2
    Сделал 2 карту 2 главы

---
+++ M4_final/Assets/Scripts/Enemy/BossZombieAI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossZombieAI : MonoBehaviour
+{
+    public float Damage;
+    public NavMeshAgent Zombie;
+    public Animator Animator;
+
+    public Transform Player;
+    public float Distanse;
+
+    bool _isNotised;
+    void Update()
+    {
+        Notise();
+        Walk();
+        if (_isNotised)
+            Attack();
+    }
+    public void AttackDamage()
+    {
+        if (Zombie.remainingDistance < Zombie.stoppingDistance)
+            Player.GetComponent<PlayerHealth>().DealDamageToPlayer(Damage);
+    }
+    private void Attack()
+    {
+        if (Zombie.remainingDistance < Zombie.stoppingDistance)
+            Animator.SetTrigger("Attack");
+    }
+    private void Walk()
+    {
+        Zombie.SetDestination(Player.position);
+    }
+    private void Notise()
+    {
+        _isNotised = false;
+
+        var direction = Player.transform.position - transform.position;
+        RaycastHit Hit;
+        if (Physics.Raycast(transform.position + Vector3.up, direction, out Hit))
+            if (Hit.collider.gameObject == Player.gameObject)
+            {
+                _isNotised = true;
+            }
+    }
+}
---
+++ M4_final/Assets/Scripts/Enemy/GoEventBoss_.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoEventBoss_ : MonoBehaviour
+{
+    public BossZombieAI ZombieAI;
+    public void AttackDamage()
+    {
+        ZombieAI.AttackDamage();
+    }
+}
